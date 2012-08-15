cd369db28e1f84c140104fb88c856b41c8e51f08
Peter Uittenbroek <uittenbroek@goldmund-wyldebeast-wunderliebe.com>
2012-08-15 11:18:35 +0200

    first commit

---
+++ buildout/eggscleaner/__init__.py
@@ -0,0 +1,74 @@
+import os
+import sys
+import logging
+import zc.buildout.easy_install
+import pkg_resources
+import shutil
+
+logger=zc.buildout.easy_install.logger
+
+
+def enable_eggscleaner(old_get_dist):
+    def get_dist(self, requirement, ws, always_unzip):
+        dists = old_get_dist(self, requirement, ws, always_unzip)
+        for dist in dists:
+            self.__used_eggs[dist.egg_name()] = dist.location
+        return dists
+    return get_dist
+
+
+def eggs_cleaner(old_logging_shutdown, eggs_directory, old_eggs_directory, extensions):
+
+    def logging_shutdown():
+        used_eggs = zc.buildout.easy_install.Installer.__used_eggs
+        eggsdirectory = os.listdir(eggs_directory)
+        move_eggs = []
+
+        for eggname in eggsdirectory:
+            fullpath = "{0}/{1}".format(eggs_directory, eggname)
+            if not fullpath in used_eggs.values():
+                is_extensions = False
+                for ext in extensions:
+                    if ext in eggname:
+                        is_extensions = True
+                        break
+                if not is_extensions:
+                    move_eggs.append(eggname)
+
+        if not os.path.exists(old_eggs_directory):
+            os.mkdir(old_eggs_directory)
+
+        print("*************** BUILDOUT EGGSCLEANER ****************")
+
+        for eggname in move_eggs:
+            oldpath = "{0}/{1}".format(eggs_directory, eggname)
+            newpath= "{0}/{1}".format(old_eggs_directory, eggname)
+            if not os.path.exists(newpath):
+                shutil.move(oldpath, newpath)
+            else:
+                shutil.rmtree(oldpath)
+            print "Moved unused egg: {0} ".format(eggname)
+
+        if not move_eggs:
+            print "No unused eggs in eggs directory"
+        print("*************** /BUILDOUT EGGSCLEANER ****************")
+
+        old_logging_shutdown()
+    return logging_shutdown
+
+def install(buildout):
+
+    eggs_directory = 'eggs-directory' in buildout['buildout'] and buildout['buildout']['eggs-directory'].strip() or None
+
+    old_eggs_directory = 'old-eggs-directory' in buildout['buildout'] and \
+                    buildout['buildout']['old-eggs-directory'].strip() or \
+                                    None
+    extensions = buildout['buildout'].get('extensions', '').split()
+
+    zc.buildout.easy_install.Installer.__used_eggs= {}
+    zc.buildout.easy_install.Installer._get_dist = enable_eggscleaner(
+                                  zc.buildout.easy_install.Installer._get_dist)
+    logging.shutdown = eggs_cleaner(logging.shutdown, eggs_directory, old_eggs_directory, extensions)
+
+
+    pass
---
+++ setup.py
@@ -0,0 +1,71 @@
+# -*- coding: utf-8 -*-
+"""
+This module contains the tool of buildout.eggscleaner
+"""
+import os
+from setuptools import setup, find_packages
+
+
+def read(*rnames):
+    return open(os.path.join(os.path.dirname(__file__), *rnames)).read()
+
+version = '0.1'
+
+long_description = (
+    read('README.txt')
+    + '\n' +
+    'Detailed Documentation\n'
+    '**********************\n'
+    + '\n' +
+    read('buildout', 'eggscleaner', 'README.txt')
+    + '\n' +
+    'Contributors\n'
+    '************\n'
+    + '\n' +
+    read('CONTRIBUTORS.txt')
+    + '\n' +
+    'Change history\n'
+    '**************\n'
+    + '\n' +
+    read('CHANGES.txt')
+    + '\n' +
+   'Download\n'
+    '********\n')
+
+entry_point = 'buildout.eggscleaner:install'
+entry_points = {
+        "zc.buildout.extension": ["default= %s" % entry_point]
+        }
+
+tests_require = ['zope.testing', 'zc.buildout']
+
+setup(name='buildout.eggscleaner',
+      version=version,
+      description="A buildout extension to move non-used eggs to a specified directory",
+      long_description=long_description,
+      # Get more strings from
+      # http://pypi.python.org/pypi?:action=list_classifiers
+      classifiers=[
+        'Framework :: Buildout',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: Zope Public License',
+        ],
+      keywords='buildout extensions eggs directory clean',
+      author='Peter Uittenbroek',
+      author_email='uittenbroek@goldmund-wyldebeast-wunderliebe.com',
+      url="''",
+      license='ZPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['buildout', ],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=['setuptools',
+                        'zc.buildout'
+                        # -*- Extra requirements: -*-
+                        ],
+      tests_require=tests_require,
+      extras_require=dict(tests=tests_require),
+      test_suite='buildout.eggscleaner.tests.test_docs.test_suite',
+      entry_points=entry_points,
+      )
